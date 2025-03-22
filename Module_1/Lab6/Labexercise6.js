a2bb70d0900b996d514698b0f39b16d79839fe88
Dayana Rosales <dca2943@yahoo.com>
2025-03-22 18:05:33 -0400

    Module 1 Lab Exercise 6

---
+++ Module_1/Lab6/Labexercise6.js
@@ -0,0 +1,20 @@
+// Create a book object
+let book = {
+  title: "The Basics of JSON",
+  description: "A book about JSON",
+  author: "Dayana Rosales",
+  pages: 888,
+};
+// Printing individual properties
+console.log(book.title); // "The Basics of JSON"
+console.log(book.description); // "A book about JSON"
+console.log(book.author); // "Dayana Rosales"
+console.log(book.pages); // 888
+
+// Print the whole book object
+console.log(book);
+
+// Update the description of the book
+book.description =
+  "An in-depth book that contains steps, strategies, and techniques you need to learn, explore, and use JSON.";
+console.log(book.description); // Updated description
