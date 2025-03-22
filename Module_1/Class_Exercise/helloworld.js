92069c2b83f7cf7795e5392b8d34e2d017296483
Dayana Rosales <dca2943@yahoo.com>
2025-03-22 01:08:41 -0400

    Helloworld.js

---
+++ Module_1/Class_Exercise/helloworld.js
@@ -0,0 +1,19 @@
+console.log("Hello World, this is in quotes'");
+
+function sayHello() {
+  console.log("Hello this is my hello function");
+
+  console.log("Hello this is my hello function");
+  console.log("Hello this is my hello function");
+  console.log("Hello this is my hello function");
+  console.log("Hello this is my hello function");
+}
+
+//this is supposed to call the function
+sayHello();
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(5, 1);
